80e3407c6b7d2d1747e82b49ec18e3611a4acbbc
Ahmet Uyar <ahmetuyar@gmail.com>
2020-04-01 08:32:35 +0000

    used k8s client logging

--- tera/t2-tera.sh
+++ tera/t2-tera.sh
@@ -32,32 +32,10 @@ ${T2_HOME}/bin/twister2 submit kubernetes jar ${T2_HOME}/examples/libexamples-ja
   -volatileDisk 4.0
 
 # the pod that end with "-0-0"
-# firstPod=$(kubectl get pods --output=jsonpath={.items..metadata.name} | grep -o "[^[:space:]]*-0-0")
 jobID=`cat $HOME/.twister2/last-job-id.txt`
 firstPod=${jobID}-0-0
 
 logFile=$(find $HOME/.twister2/${jobID} -name "worker0-*")
-
-# wait for the first pod to become Running
-# until kubectl get pod $firstPod 2> /dev/null | grep Running; do
-#   sleep 1
-#  echo waiting pod to start
-#done
-
-########################################
-# wait until sorting finished
-#logFile=${logsDir}/${firstPod}.log
-# if unbuffer exists, use it
-#if hash unbuffer 2>/dev/null; then
-#  unbuffer kubectl logs --follow $firstPod 2>&1 | tee ${logFile}
-#else
-#  kubectl logs --follow $firstPod 2>&1 | tee ${logFile}
-#  echo Getting $firstPod logs to $logFile
-#  echo waiting it to complete...
-#  kubectl logs --follow $firstPod > ${logFile}
-#fi
-
-#echo saved the log file to: ${logFile}
 
 # get delay and write it to file
 delayLine=$(cat $logFile | grep "Total time for all iterations")
@@ -67,5 +45,5 @@ delay=${trimmedLine##* }
 echo -e "${jobID}\t${workers}\t${totalData}\t${delay}" >> $outFile
 echo -e "${jobID}\t${workers}\t${totalData}\t${delay}"
 
-# kill the job
+# no need to kill the job, it is deleted if it completes successfully
 # ${T2_HOME}/bin/twister2 kill kubernetes $jobID
