5c561eb1f04f3347e632675557ab8f85ad6e5c29
Ahmet Uyar <ahmetuyar@gmail.com>
2020-04-09 10:41:42 +0000

    modified standalone scripts for equal number of workers per node

--- tera/t2/standalone/many.sh
+++ tera/t2/standalone/many.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-for i in 1 2 4 8 16 32 64 128 256 512 768 1024 ; do
 
-  # run two times
-  for j in 1 2; do
-    echo "Running $i workers ..."
-    ./mem-tera.sh $i 1
-#    ./disk-tera.sh $i 1
+nodes=16
+
+for workersPerNode in 1 3 5 10 15 20 25 30 ; do
+
+  # update nodes file with $i slots
+  ./update-nodes.sh conf/standalone/nodes $workersPerNode
+
+  # run three times
+  for j in 1 2 3; do
+    echo "Running with $workersPerNode workers per node..."
+    ./disk-tera.sh $nodes $workersPerNode
+    
   done
 
 done
