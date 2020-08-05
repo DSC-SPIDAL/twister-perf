5641b0879ebe9f48ef27f315be29aa5943e989a3
Ahmet Uyar <ahmetuyar@gmail.com>
2020-08-05 18:56:49 +0000

    updated twister2 version to 0.8.0

--- tera/params.sh
+++ tera/params.sh
@@ -1,10 +1,10 @@
 # update all three parameters
 
-export SPARK_HOME="${HOME}/projects/spark-perf/spark-3.0.0-preview2-bin-hadoop2.7"
+export SPARK_HOME="${HOME}/spark-perf/spark-3.0.0-preview2-bin-hadoop2.7"
 
 # can get master ip address by following command
 # but could not get it work with variable assignment strangely
 # master=$(kubectl cluster-info | head -n 1 | awk '{ print $NF }')
 #K8S_MASTER=k8s://https://ip:port
 
-export T2_HOME=${HOME}/twister2/twister2-0.6.0-SNAPSHOT
+export T2_HOME=${HOME}/twister2/twister2-0.8.0-SNAPSHOT
