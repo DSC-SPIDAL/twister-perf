ce47f54405d7b8b1b37020696ce0756da582c753
supunkamburugamuva <supun06@gmail.com>
2020-10-20 13:10:14 -0400

    fixing an issue with setting schema

--- src/main/java/org/twister2/perf/join/spark/JoinJobRandom.java
+++ src/main/java/org/twister2/perf/join/spark/JoinJobRandom.java
@@ -24,6 +24,7 @@ public class JoinJobRandom {
     Configuration configuration = new Configuration();
     configuration.setInt("parallel", Integer.parseInt(args[0]));
     configuration.setInt("records", Integer.parseInt(args[1]));
+    boolean persist = Boolean.parseBoolean(args[2]);
 
     JavaSparkContext sc = new JavaSparkContext(conf);
 
@@ -35,18 +36,30 @@ public class JoinJobRandom {
     Dataset<Row> ds1 = sqlContext.createDataset(JavaPairRDD.toRDD(input1),
         Encoders.tuple(Encoders.LONG(), Encoders.LONG())).toDF("key", "value");
     LOG.info("Total elements 1: " + ds1.count());
-    Dataset<Row> ds1Persist = ds1.persist(StorageLevel.MEMORY_AND_DISK());
+    Dataset<Row> ds1Persist = null;
+    if (persist) {
+      ds1Persist = ds1.persist(StorageLevel.MEMORY_AND_DISK());
+    }
 
 
     JavaPairRDD<Long, Long> input2 = sc.newAPIHadoopRDD(configuration, LongInputFormat.class, Long.class, Long.class);
     Dataset<Row> ds2 = sqlContext.createDataset(JavaPairRDD.toRDD(input2),
         Encoders.tuple(Encoders.LONG(), Encoders.LONG())).toDF("key", "value");
     LOG.info("Total elements 2: " + ds1.count());
-    Dataset<Row> ds2Persist =ds2.persist(StorageLevel.MEMORY_AND_DISK());
+    Dataset<Row> ds2Persist = null;
+    if (persist) {
+      ds2Persist = ds2.persist(StorageLevel.MEMORY_AND_DISK());
+    }
 
     long start = System.nanoTime();
-    Dataset<Row> join = ds1Persist.alias("ds1").join(ds2Persist.alias("ds2"), ds1Persist.col("key")
-        .equalTo(ds2Persist.col("key")), "inner").select();
+    Dataset<Row> join;
+    if (persist) {
+      join = ds1Persist.alias("ds1").join(ds2Persist.alias("ds2"), ds1Persist.col("key")
+          .equalTo(ds2Persist.col("key")), "inner").select();
+    } else {
+      join = ds1.alias("ds1").join(ds2.alias("ds2"), ds1.col("key")
+          .equalTo(ds2.col("key")), "inner").select();
+    }
     LOG.info("Final total: " + join.count());
     LOG.info("Time: " + (System.nanoTime() - start) / 1000000);
 
